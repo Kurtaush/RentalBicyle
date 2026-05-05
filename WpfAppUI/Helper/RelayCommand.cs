abcf324f74b819a90d4a665b33edd6d420b0b378
Александр <alex@email.com>
2026-05-05 11:54:08 +0900

    05.05 v0.2

---
+++ WpfAppUI/Helper/RelayCommand.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfAppUI.Helper
+{
+    // Это стандартный RelayCommand для WPF.
+    // Он оборачивает обычный метод в объект, который умеет сообщать UI, можно ли его выполнить (CanExecute) и что делать (Execute).
+    // Убирает необходимость писать обработчики в .xaml.cs
+    public class RelayCommand : ICommand
+    {
+        private Action<object> execute;
+        private Func<object, bool> canExecute;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+        public RelayCommand(Action<object> execute, Func<object, bool>
+        canExecute = null)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return this.canExecute == null || this.canExecute(parameter);
+        }
+        public void Execute(object parameter)
+        {
+            this.execute(parameter);
+        }
+    }
+}
