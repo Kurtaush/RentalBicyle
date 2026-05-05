c32d54a7eda8ddf3dc14184a564373a981ee022d
Александр <alex@email.com>
2026-05-05 14:53:01 +0900

    05.05 v0.7

--- WpfAppUI/View/AdminWindow.xaml.cs
+++ WpfAppUI/View/AdminWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using WpfAppUI.ViewAdmin;
 
 namespace WpfAppUI
 {
@@ -22,6 +23,53 @@ namespace WpfAppUI
         public AdminWindow()
         {
             InitializeComponent();
+        }
+
+        private void Bicycle_Click(object sender, RoutedEventArgs e)
+        {
+            BicyclesWindow wBicycle = new BicyclesWindow();
+            wBicycle.Show();
+        }
+
+        private void Client_Click(object sender, RoutedEventArgs e)
+        {
+            ClientsWindow wClient = new ClientsWindow();
+            wClient.Show();
+        }
+
+        private void Payment_Click(object sender, RoutedEventArgs e)
+        {
+            PaymentsWindow wPayment = new PaymentsWindow();
+            wPayment.Show();
+        }
+
+        private void Rental_Click(object sender, RoutedEventArgs e)
+        {
+            RentalsWindow wRental = new RentalsWindow();
+            wRental.Show();
+        }
+
+        private void Station_Click(object sender, RoutedEventArgs e)
+        {
+            StationsWindow wStation = new StationsWindow();
+            wStation.Show();
+        }
+
+        private void Tariff_Click(object sender, RoutedEventArgs e)
+        {
+            TariffsWindow wTariff = new TariffsWindow();
+            wTariff.Show();
+        }
+
+        private void User_Click(object sender, RoutedEventArgs e)
+        {
+            UsersWindow wUser = new UsersWindow();
+            wUser.Show();
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
         }
     }
 }
