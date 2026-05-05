0a9fc79961d50c2970bc89f05dd520f1bcdf2723
Александр <sahsaxxxz123@yandex.ru>
2026-05-05 14:39:22 +0900

    Обнова с админом

--- WpfAppUI/MainWindow.xaml.cs
+++ WpfAppUI/MainWindow.xaml.cs
@@ -32,32 +32,32 @@ namespace WpfAppUI
 
         public void UpdateUI()
         {
-                if (btnLogin != null) btnLogin.Visibility = Visibility.Collapsed;
-                if (btnReg != null) btnReg.Visibility = Visibility.Collapsed;
-                if (textPass != null) textPass.Visibility = Visibility.Collapsed;
-                if (btnLogin != null) btnBalance.Visibility = Visibility.Collapsed;
-                if (btnReg != null) btnHistory.Visibility = Visibility.Collapsed;
-                if (btnTariff != null) btnTariff.Visibility = Visibility.Collapsed;
-                if (textInfo != null) textInfo.Visibility = Visibility.Collapsed;
-                if (btnExitAcc != null) btnExitAcc.Visibility = Visibility.Collapsed;
+                btnLogin.Visibility = Visibility.Collapsed;
+                btnReg.Visibility = Visibility.Collapsed;
+                textPass.Visibility = Visibility.Collapsed;
+                btnBalance.Visibility = Visibility.Collapsed;
+                btnHistory.Visibility = Visibility.Collapsed;
+                btnTariff.Visibility = Visibility.Collapsed;
+                textInfo.Visibility = Visibility.Collapsed;
+                btnExitAcc.Visibility = Visibility.Collapsed;
             
 
             if (CurrentUser.Identity != null)
             {
                 if (CurrentUser.Identity.Role == "client")
                 {
-                    if (btnLogin != null) btnBalance.Visibility = Visibility.Visible;
-                    if (btnReg != null) btnHistory.Visibility = Visibility.Visible;
-                    if (btnTariff != null) btnTariff.Visibility = Visibility.Visible;
-                    if (textInfo != null) textInfo.Visibility = Visibility.Visible;
-                    if (btnExitAcc != null) btnExitAcc.Visibility = Visibility.Visible;
+                    btnBalance.Visibility = Visibility.Visible;
+                    btnHistory.Visibility = Visibility.Visible;
+                    btnTariff.Visibility = Visibility.Visible;
+                    textInfo.Visibility = Visibility.Visible;
+                    btnExitAcc.Visibility = Visibility.Visible;
                 }
             }
             else
             {
-                if (btnLogin != null) btnLogin.Visibility = Visibility.Visible;
-                if (textPass != null) textPass.Visibility = Visibility.Visible;
-                if (btnReg != null) btnReg.Visibility = Visibility.Visible;
+                btnLogin.Visibility = Visibility.Visible;
+                textPass.Visibility = Visibility.Visible;
+                btnReg.Visibility = Visibility.Visible;
             }
         }
 
