cebeb1b0433497ceda5f38d8b977ab69ff167c91
Александр <sahsaxxxz123@yandex.ru>
2026-05-06 08:47:24 +0900

    Добавлении метки и миграции

---
+++ WpfAppUI/Migrations/202605052345490_UpdateClientTable.cs
@@ -0,0 +1,18 @@
+﻿namespace WpfAppUI.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateClientTable : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Clients", "Balance", c => c.Int(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Clients", "Balance");
+        }
+    }
+}
